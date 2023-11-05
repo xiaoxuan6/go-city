ca758171d55577464c1a5a4e9c2c3b06186d0c5f
xiaoxuan6 <1527736751@qq.com>
2023-11-05 09:46:37 +0800

    feat: Add .goreleaser.haml

--- go.mod
+++ go.mod
@@ -54,13 +54,14 @@ require (
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/term v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"os"
 )
 
+var Version string
+
 func main() {
 	app := &cli.App{
 		Name:        "go-city",
@@ -17,6 +19,7 @@ func main() {
 			{
 				Name:        "sync",
 				Aliases:     []string{"s"},
+				Usage:       "sync data to database, data source is jd",
 				Description: figure.NewFigure("Go City Sync", "", true).String() + "同步省市区到数据库，数据来源于京东",
 				Flags:       command.Flags,
 				Action:      command.Run,
@@ -24,9 +27,20 @@ func main() {
 			{
 				Name:        "sync-tjj",
 				Aliases:     []string{"tjj"},
+				Usage:       "sync data to database, data source is 统计局",
 				Description: figure.NewFigure("Go City Sync-Tjj", "", true).String() + "同步省市区到数据库，数据来源于国家统计局",
 				Flags:       command.Flags,
 				Action:      command.Exec,
+			},
+			{
+				Name:        "version",
+				Aliases:     []string{"v"},
+				Usage:       "show go city version",
+				Description: figure.NewFigure("Go City Version", "", true).String() + "显示版本号",
+				Action: func(context *cli.Context) error {
+					logrus.Info("go-city version: ", Version)
+					return nil
+				},
 			},
 		},
 	}
